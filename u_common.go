0dc8a71c6cebc31eb7da44c59321fd6763b34de3
vimbing <jakubbubak9@gmail.com>
2025-10-04 23:44:32 +0200

    added chrome 140

--- u_common.go
+++ u_common.go
@@ -644,6 +644,7 @@ var (
 	HelloChrome_131 = ClientHelloID{helloChrome, "131", nil, nil}
 	// Chrome w/ New ALPS codepoint
 	HelloChrome_133 = ClientHelloID{helloChrome, "133", nil, nil}
+	HelloChrome_140 = ClientHelloID{helloChrome, "140", nil, nil}
 
 	HelloIOS_Auto = HelloIOS_14
 	HelloIOS_11_1 = ClientHelloID{helloIOS, "111", nil, nil} // legacy "111" means 11.1
